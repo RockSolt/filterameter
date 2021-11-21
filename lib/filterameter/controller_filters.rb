9610c9599a08cb15504ab593277fa63f8173f044
Todd Kummer <todd@rockridgesolutions.com>
2021-11-21 11:21:58 -0800

    Extract FilterRegistry from ControllerFilters

--- lib/filterameter/controller_filters.rb
+++ lib/filterameter/controller_filters.rb
@@ -7,6 +7,7 @@ require 'action_dispatch'
 require 'action_controller/metal/live'
 require 'action_controller/metal/strong_parameters'
 
+require 'filterameter/filter_registry'
 require 'filterameter/filter_factory'
 require 'filterameter/filter_declaration'
 require 'filterameter/log_subscriber'
@@ -21,23 +22,15 @@ module Filterameter
   class ControllerFilters
     attr_writer :query_variable_name
 
+    delegate :add_filter, to: :registry
+
     def initialize(controller_name, controller_path)
       @controller_name = controller_name
       @controller_path = controller_path
-      @declarations = {}
-      @ranges = {}
-      @filters = Hash.new { |hash, key| hash[key] = filter_factory.build(@declarations[key]) }
     end
 
     def model_class=(model_class)
       @model_class = model_class.is_a?(String) ? model_class.constantize : model_class
-    end
-
-    def add_filter(parameter_name, options)
-      @declarations[parameter_name.to_s] =
-        Filterameter::FilterDeclaration.new(parameter_name, options).tap do |fd|
-          add_declarations_for_range(fd, options, parameter_name) if fd.range_enabled?
-        end
     end
 
     def query_variable_name
@@ -48,7 +41,7 @@ module Filterameter
       valid_filters(filter_params)
         .tap { |parameters| convert_min_and_max_to_range(parameters) }
         .reduce(starting_query || model_class.all) do |query, (name, value)|
-        @filters[name].apply(query, value)
+        registry.fetch(name).apply(query, value)
       end
     end
 
@@ -62,8 +55,8 @@ module Filterameter
     end
 
     # lazy so that model_class can be optionally set
-    def filter_factory
-      @filter_factory ||= Filterameter::FilterFactory.new(model_class)
+    def registry
+      @registry ||= Filterameter::FilterRegistry.new(Filterameter::FilterFactory.new(model_class))
     end
 
     def valid_filters(filter_params)
@@ -74,7 +67,7 @@ module Filterameter
 
     # if both min and max are present in the query parameters, replace with range
     def convert_min_and_max_to_range(parameters)
-      @ranges.each do |attribute_name, min_max_names|
+      registry.ranges.each do |attribute_name, min_max_names|
         next unless min_max_names.values.all? { |min_max_name| parameters[min_max_name].present? }
 
         parameters[attribute_name] = Range.new(parameters.delete(min_max_names[:min]),
@@ -116,33 +109,11 @@ module Filterameter
     end
 
     def declared_parameter_names
-      @declared_parameter_names ||= @declarations.keys
+      registry.filter_names
     end
 
     def validator_class
-      @validator_class ||= Filterameter::ParametersBase.build_sub_class(@declarations.values)
-    end
-
-    # if range is enabled, then in addition to the attribute filter this also adds min and/or max filters
-    def add_declarations_for_range(attribute_declaration, options, parameter_name)
-      add_range_minimum(parameter_name, options) if attribute_declaration.range? || attribute_declaration.minimum?
-      add_range_maximum(parameter_name, options) if attribute_declaration.range? || attribute_declaration.maximum?
-      capture_range_declaration(parameter_name) if attribute_declaration.range?
-    end
-
-    def add_range_minimum(parameter_name, options)
-      @declarations["#{parameter_name}_min"] = Filterameter::FilterDeclaration.new(parameter_name,
-                                                                                   options.merge(range: :min_only))
-    end
-
-    def add_range_maximum(parameter_name, options)
-      @declarations["#{parameter_name}_max"] = Filterameter::FilterDeclaration.new(parameter_name,
-                                                                                   options.merge(range: :max_only))
-    end
-
-    # memoizing these makes it easier to spot and replace ranges in query parameters; see convert_min_and_max_to_range
-    def capture_range_declaration(name)
-      @ranges[name] = { min: "#{name}_min", max: "#{name}_max" }
+      @validator_class ||= Filterameter::ParametersBase.build_sub_class(registry.filter_declarations)
     end
   end
 end
---
+++ lib/filterameter/filter_registry.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+module Filterameter
+  # Filters
+  #
+  # Class FilterRegistry is a collection of the filters. It captures the filter declarations when classes are loaded,
+  # then uses the injected FilterFactory to build the filters on demand as they are needed.
+  class FilterRegistry
+    attr_reader :ranges
+
+    def initialize(filter_factory)
+      @filter_factory = filter_factory
+      @declarations = {}
+      @ranges = {}
+      @filters = {}
+    end
+
+    def add_filter(parameter_name, options)
+      @declarations[parameter_name.to_s] = Filterameter::FilterDeclaration.new(parameter_name, options).tap do |fd|
+        add_declarations_for_range(fd, options, parameter_name) if fd.range_enabled?
+      end
+    end
+
+    def fetch(name)
+      @filters.fetch(name) do
+        @filters[name] = @filter_factory.build(@declarations[name])
+      end
+    end
+
+    def filter_names
+      @declarations.keys
+    end
+
+    def filter_declarations
+      @declarations.values
+    end
+
+    private
+
+    # if range is enabled, then in addition to the attribute filter this also adds min and/or max filters
+    def add_declarations_for_range(attribute_declaration, options, parameter_name)
+      add_range_minimum(parameter_name, options) if attribute_declaration.range? || attribute_declaration.minimum?
+      add_range_maximum(parameter_name, options) if attribute_declaration.range? || attribute_declaration.maximum?
+      capture_range_declaration(parameter_name) if attribute_declaration.range?
+    end
+
+    def add_range_minimum(parameter_name, options)
+      @declarations["#{parameter_name}_min"] = Filterameter::FilterDeclaration.new(parameter_name,
+                                                                                   options.merge(range: :min_only))
+    end
+
+    def add_range_maximum(parameter_name, options)
+      @declarations["#{parameter_name}_max"] = Filterameter::FilterDeclaration.new(parameter_name,
+                                                                                   options.merge(range: :max_only))
+    end
+
+    def capture_range_declaration(name)
+      @ranges[name] = { min: "#{name}_min", max: "#{name}_max" }
+    end
+  end
+end
