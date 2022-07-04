a78b8d6dc3b2c70e157b7845f2bc9f0730a3a15e
Todd Kummer <todd@rockridgesolutions.com>
2022-07-04 15:59:13 -0700

    Update versions for rubocop, rubocop-rails

--- spec/models/vendor_spec.rb
+++ spec/models/vendor_spec.rb
@@ -11,11 +11,11 @@ RSpec.describe Vendor, type: :model do
     end
 
     it 'Happy House in five days' do
-      expect(described_class.ships_by(Time.zone.now + 5.days)).to contain_exactly(vendors(:happy_house))
+      expect(described_class.ships_by(5.days.from_now)).to contain_exactly(vendors(:happy_house))
     end
 
     it 'all ships by next week' do
-      expect(described_class.ships_by(Time.zone.now + 10.days).size).to eq 2
+      expect(described_class.ships_by(10.days.from_now).size).to eq 2
     end
   end
 end
