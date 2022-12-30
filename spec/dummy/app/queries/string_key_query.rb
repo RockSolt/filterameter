f51c14a2a9051bf70eef0f50b799f79e154633a2
Todd Kummer <todd@rockridgesolutions.com>
2022-12-30 14:38:00 -0800

    Use string keys for filter registry and criteria
    - filter registry converts keys to strings in both add and fetch
    - query builder converts criteria to string keys

---
+++ spec/dummy/app/queries/string_key_query.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# = String Key Query
+#
+# Class StringKeyQuery builds a query using strings as keys.
+class StringKeyQuery
+  include Filterameter::DeclarativeFilters
+
+  model 'Shirt'
+
+  filter 'color'
+  filter 'size', validates: { inclusion: { in: %w[Small Medium Large], allow_multiple_values: true } }
+  filter 'brand_name', association: :brand, name: :name
+  filter 'on_sale', association: :price, validates: [{ numericality: { greater_than: 0 } },
+                                                     { numericality: { less_than: 100 } }]
+  filter 'color_type_ahead', name: :color, partial: { match: :from_start }
+  filter 'fuzzy_color', name: :color, partial: true
+  filter 'color_client_search', name: :color, partial: { match: :dynamic }
+  filter 'case_sensitive_color', name: :color, partial: { case_sensitive: true }
+  filter 'price', name: :current, association: :price, range: true
+end
