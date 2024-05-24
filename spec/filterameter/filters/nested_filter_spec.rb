26fff40e556382adeaeacd7c43191586b7bebeb4
Todd Kummer <todd@rockridgesolutions.com>
2024-05-24 11:04:42 -0700

    Add fixture call to nested associaitons test

--- spec/filterameter/filters/nested_filter_spec.rb
+++ spec/filterameter/filters/nested_filter_spec.rb
@@ -3,6 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe Filterameter::Filters::NestedFilter do
+  fixtures :shirts, :brands
+
   let(:filter) { described_class.new([:brand], Brand, Filterameter::Filters::AttributeFilter.new(:name)) }
   let(:result) { filter.apply(Shirt.all, 'Happy Shirts') }
 
@@ -14,31 +16,35 @@ RSpec.describe Filterameter::Filters::NestedFilter do
     expect(result.where_values_hash('brands')).to match('name' => 'Happy Shirts')
   end
 
-  context 'with multi-level association for attribute filter' do
-    let(:filter) { described_class.new(%i[brand vendor], Vendor, Filterameter::Filters::AttributeFilter.new(:name)) }
-    let(:result) { filter.apply(Shirt.all, 'Happy House') }
+  describe 'multi-level associations' do
+    fixtures :vendors
 
-    it 'includes builds nested hash for joins' do
-      expect(result.joins_values).to contain_exactly({ brand: { vendor: {} } })
+    context 'with attribute filter' do
+      let(:filter) { described_class.new(%i[brand vendor], Vendor, Filterameter::Filters::AttributeFilter.new(:name)) }
+      let(:result) { filter.apply(Shirt.all, 'Happy House') }
+
+      it 'includes builds nested hash for joins' do
+        expect(result.joins_values).to contain_exactly({ brand: { vendor: {} } })
+      end
+
+      it 'applies nested filter' do
+        expect(result.map(&:brand).map(&:vendor).map(&:name).uniq)
+          .to eq(['Happy House']), "The following query did not return the expected results: #{result.to_sql}"
+      end
     end
 
-    it 'applies nested filter' do
-      expect(result.map(&:brand).map(&:vendor).map(&:name).uniq)
-        .to eq(['Happy House']), "The following query did not return the expected results: #{result.to_sql}"
-    end
-  end
+    context 'with scope filter' do
+      let(:filter) { described_class.new(%i[brand vendor], Vendor, Filterameter::Filters::ScopeFilter.new(:ships_by)) }
+      let(:result) { filter.apply(Shirt.all, 5.days.from_now) }
 
-  context 'with multi-level association for scope filter' do
-    let(:filter) { described_class.new(%i[brand vendor], Vendor, Filterameter::Filters::ScopeFilter.new(:ships_by)) }
-    let(:result) { filter.apply(Shirt.all, 5.days.from_now) }
+      it 'includes builds nested hash for joins' do
+        expect(result.joins_values).to contain_exactly({ brand: { vendor: {} } })
+      end
 
-    it 'includes builds nested hash for joins' do
-      expect(result.joins_values).to contain_exactly({ brand: { vendor: {} } })
-    end
-
-    it 'applies nested filter' do
-      expect(result.map(&:brand).map(&:vendor).map(&:name).uniq)
-        .to eq(['Happy House']), "The following query did not return the expected results: #{result.to_sql}"
+      it 'applies nested filter' do
+        expect(result.map(&:brand).map(&:vendor).map(&:name).uniq)
+          .to eq(['Happy House']), "The following query did not return the expected results: #{result.to_sql}"
+      end
     end
   end
 end
