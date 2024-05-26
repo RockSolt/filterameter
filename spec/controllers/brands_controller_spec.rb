1a25a0dd38908defe57c368505e7548f7e63b319
Todd Kummer <todd@rockridgesolutions.com>
2024-05-26 11:15:14 -0700

    Add controllers for vendors and brands

---
+++ spec/controllers/brands_controller_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe BrandsController, type: :controller do
+  fixtures :brands
+
+  let(:brand_with_blue_shirts) { brands(:happy_shirts) }
+  let(:response_body) { response.parsed_body }
+
+  context 'when looking for brands with blue shirts' do
+    let(:response) { get :index, params: { filter: { color: 'Blue' } } }
+
+    it { expect(response).to be_successful }
+    it { expect(response_body.size).to eq 1 }
+    it('finds Happy Shirts') { expect(response_body.first).to include('name' => brand_with_blue_shirts.name) }
+  end
+
+  context 'when looking for brands with large shirts' do
+    let(:response) { get :index, params: { filter: { size: 'Large' } } }
+
+    it { expect(response).to be_successful }
+    it { expect(response_body.size).to eq 2 }
+  end
+end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :brands, only: :index
   resources :shirts
+  resources :vendors, only: :index
 end
