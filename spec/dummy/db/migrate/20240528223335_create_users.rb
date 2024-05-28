8ca09b195355fc6d653a5858054dc23df010d2c1
Todd Kummer <todd@rockridgesolutions.com>
2024-05-28 15:35:17 -0700

    Add User

---
+++ spec/dummy/db/migrate/20240528223335_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.boolean :active
+
+      t.timestamps
+    end
+  end
+end
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_28_221556) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_28_223335) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_28_221556) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["brand_id"], name: "index_shirts_on_brand_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.boolean "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "vendors", force: :cascade do |t|
