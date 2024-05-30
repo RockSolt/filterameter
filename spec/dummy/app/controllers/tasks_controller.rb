fff3e324f56bba4a8796af2b204185d8010bce02
Todd Kummer <todd@rockridgesolutions.com>
2024-05-30 10:33:01 -0700

    Tests for partial filters

--- spec/dummy/app/controllers/tasks_controller.rb
+++ spec/dummy/app/controllers/tasks_controller.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
+  filter :description, partial: true
+  filter :description_starts_with, name: :description, partial: :from_start
+  filter :description_case_sensitive, name: :description, partial: { case_sensitive: true }
+  filter :description_dynamic, name: :description, partial: { match: :dynamic }
+
   def index
-    @tasks = Task.all
+    @tasks = build_query
 
     render json: @tasks
   end
---
+++ spec/requests/partial_filters_spec.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Partial filters', type: :request do
+  fixtures :tasks
+
+  context 'with defaults' do
+    before { get '/tasks', params: { filter: { description: 'beans' } } }
+
+    it 'returns the correct number of rows' do
+      count = Task.where("description like '%beans%'").count
+      expect(response.parsed_body.size).to eq count
+    end
+
+    it 'returns Grind coffee beans' do
+      expect(response).to have_http_status(:success)
+      expect(response.parsed_body).to include(a_hash_including('description' => tasks(:grind_coffee_beans).description))
+    end
+  end
+
+  context 'with from start option' do
+    before { get '/tasks', params: { filter: { description_starts_with: 'Add beans' } } }
+
+    it 'returns the correct number of rows' do
+      count = Task.where("description like 'Add beans%'").count
+      expect(response.parsed_body.size).to eq count
+    end
+
+    it 'returns Add beans and water to french press' do
+      expect(response).to have_http_status(:success)
+      expect(response.parsed_body).to include(a_hash_including('description' => tasks(:french_press).description))
+    end
+  end
+
+  context 'with case sensitive option' do
+    before { get '/tasks', params: { filter: { description_case_sensitive: 'Add beans' } } }
+
+    it 'returns the correct number of rows' do
+      count = Task.where("description ilike 'Add beans%'").count
+      expect(response.parsed_body.size).to eq count
+    end
+
+    it 'returns Add beans and water to french press' do
+      expect(response).to have_http_status(:success)
+      expect(response.parsed_body).to include(a_hash_including('description' => tasks(:french_press).description))
+    end
+  end
+
+  context 'with case sensitive option and no match' do
+    before { get '/tasks', params: { filter: { description_case_sensitive: 'add beans' } } }
+
+    it 'returns the no rows' do
+      expect(response.parsed_body.size).to be_zero
+    end
+  end
+
+  context 'with dynamic option' do
+    before { get '/tasks', params: { filter: { description_dynamic: '%beans%' } } }
+
+    it 'returns the correct number of rows' do
+      count = Task.where("description like '%beans%'").count
+      expect(response.parsed_body.size).to eq count
+    end
+
+    it 'returns Grind coffee beans' do
+      expect(response).to have_http_status(:success)
+      expect(response.parsed_body).to include(a_hash_including('description' => tasks(:grind_coffee_beans).description))
+    end
+  end
+end
