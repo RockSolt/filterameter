22e883f9982a6fefb66ee055774954d7699b1f63
Todd Kummer <todd@rockridgesolutions.com>
2024-05-31 13:14:58 -0700

    Handle enum change across Rails versions

--- spec/dummy/app/models/project.rb
+++ spec/dummy/app/models/project.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 class Project < ApplicationRecord
-  enum :priority, %i[low medium high], suffix: true
+  # enum signature changed in Rails 7
+  if method(:enum).arity == 1
+    enum priority: %i[low medium high], _suffix: true
+  else
+    enum :priority, %i[low medium high], suffix: true
+  end
 
   def self.in_progress(as_of)
     where(start_date: ..as_of)
