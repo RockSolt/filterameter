934d292a49ca71d0432fa4aafda89c3205252eb3
Todd Kummer <todd@rockridgesolutions.com>
2024-06-25 10:02:40 -0700

    Add Requested Sort

---
+++ lib/filterameter/helpers/requested_sort.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Filterameter
+  module Helpers
+    # = Reqested Sort
+    #
+    # Class RequestedSort parses the name and direction from a sort segment.
+    class RequestedSort
+      SIGN_AND_NAME = /(?<sign>[+|-]?)(?<name>\w+)/
+      attr_reader :name, :direction
+
+      def self.parse(sort)
+        parsed = sort.match SIGN_AND_NAME
+
+        new(parsed['name'],
+            parsed['sign'] == '-' ? :desc : :asc)
+      end
+
+      def initialize(name, direction)
+        @name = name
+        @direction = direction
+      end
+    end
+  end
+end
