285f786203f87a9d4997f1de9a9c0f62698cec0d
Todd Kummer <todd@rockridgesolutions.com>
2024-07-17 09:59:16 -0700

    Add validation to AttributeFilter

--- lib/filterameter/filters/attribute_filter.rb
+++ lib/filterameter/filters/attribute_filter.rb
@@ -6,12 +6,22 @@ module Filterameter
     #
     # Class AttributeFilter leverages ActiveRecord's where query method to add criteria for an attribute.
     class AttributeFilter
+      include Filterameter::Errors
+
       def initialize(attribute_name)
         @attribute_name = attribute_name
       end
 
       def apply(query, value)
         query.where(@attribute_name => value)
+      end
+
+      private
+
+      def validate(model)
+        return if model.attribute_method? @attribute_name
+
+        @errors << "Attribute '#{@attribute_name}' does not exist on #{model.name}"
       end
     end
   end
--- spec/filterameter/filters/attribute_filter_spec.rb
+++ spec/filterameter/filters/attribute_filter_spec.rb
@@ -3,11 +3,29 @@
 require 'rails_helper'
 
 RSpec.describe Filterameter::Filters::AttributeFilter do
-  let(:filter) { described_class.new(:color) }
-  let(:query) { class_spy('ActiveRecord::Base') }
-
   it 'applies filter to query' do
+    filter = described_class.new(:color)
+    query = class_spy('ActiveRecord::Base')
+
     filter.apply(query, 'blue')
     expect(query).to have_received(:where).with(color: 'blue')
+  end
+
+  it 'is valid with valid attribute' do
+    filter = described_class.new(:name)
+    expect(filter.valid?(Activity)).to be true
+  end
+
+  context 'with invalid attribute' do
+    let(:filter) { described_class.new(:not_a_thing) }
+
+    it 'is not valid' do
+      expect(filter.valid?(Activity)).to be false
+    end
+
+    it 'reports errors' do
+      filter.valid?(Activity)
+      expect(filter.errors).to contain_exactly "Attribute 'not_a_thing' does not exist on Activity"
+    end
   end
 end
