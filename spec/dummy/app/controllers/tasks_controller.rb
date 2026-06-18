390dc53499b36cf2eee56787f668f4eff0fbf876
Todd Kummer <todd@rockridgesolutions.com>
2026-06-18 11:28:27 -0700

    Add converter test for Attribute filters

--- spec/dummy/app/controllers/tasks_controller.rb
+++ spec/dummy/app/controllers/tasks_controller.rb
@@ -2,6 +2,9 @@
 
 class TasksController < ApplicationController
   filter :completed
+  filter :yes_no_completed, name: :completed do |value|
+    value == 'YES'
+  end
   filter :description, partial: true
   filter :description_starts_with, name: :description, partial: :from_start
   filter :description_case_sensitive, name: :description, partial: { case_sensitive: true }
